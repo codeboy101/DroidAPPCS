8169c8e3149dd1c46b7cfd1a61b9650d3ba8c239
tushar192 <tushar192@TUSHAR>
2016-10-29 10:50:41 +0530

    Add project files.

---
+++ DroidApp/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+
+namespace DroidApp
+{
+    [Activity(Label = "DroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            callButton.Enabled = false;
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                translatedNumber = Core.CodeTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber)) {
+                    callButton.Text = "call";
+                    callButton.Enabled = false;
+                } else
+                {
+                    callButton.Text = "Call : " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                callDialog.Show();
+            };
+        }
+    }
+}
+
